695f6e7bb13ed8b22deec198caee20d308a86a9d
Muhammad Kautsar Aqsa <60873504+KautsarAqsa@users.noreply.github.com>
2023-02-07 11:46:00 +0700

    change ref in models

--- models/public_2/aggregate.sql
+++ models/public_2/aggregate.sql
@@ -6,5 +6,5 @@
 --QUERY
 SELECT 
 source, customer_id, customer_name, city, SUM(payment) AS total_payment
-FROM {{ ref('transform') }}
+FROM {{ ref('public_2','transform') }}
 GROUP BY source, customer_id, customer_name, city
