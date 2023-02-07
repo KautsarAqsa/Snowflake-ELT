a8b778a43a9524de20a75bb187d81f95a08e8be6
Muhammad Kautsar Aqsa <60873504+KautsarAqsa@users.noreply.github.com>
2023-02-07 11:50:50 +0700

    change filename

--- models/public_2/aggregate_2.sql
+++ models/public_2/aggregate_2.sql
@@ -6,5 +6,5 @@
 --QUERY
 SELECT 
 source, customer_id, customer_name, city, SUM(payment) AS total_payment
-FROM {{ ref('public_2','transform') }}
+FROM {{ ref('public_2','transform_2') }}
 GROUP BY source, customer_id, customer_name, city
